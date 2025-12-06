32ed85b8471dc43492d750135632b3dc69bea6b2
chloe-c317 <chloe.chinault@gmail.com>
2025-12-06 12:06:22 -0600

    Update and rename UtilsTest to UtilsTest.java
    Added multiple test cases for name and age validation.

---
+++ src/test/java/UtilsTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilsTest{
+  @Test
+  void testCheckNameValid(){
+    assertTrue(Utils.checkName("Alice"));
+  }
+
+  @Test
+  void testCheckNameEmpty(){
+    assertFalse(Utils.checkName(""));
+  }
+
+  @Test
+  void testCheckNameNull(){
+    assertFalse(Utils.checkName(null));
+  }
+
+  @Test
+  void testIsValidNegativeAge(){
+    assertFalse(Utils.isValidAge(-5));
+  }
+
+  @Test
+  void testIsValidZeroAge(){
+    assertTrue(Utils.isValidAge(0));
+  }
+
+  @Test
+  void testIsValidAgeNormal(){
+    assertTrue(Utils.isValidAge(25));
+  }
+
+  @Test
+  void testIsValidAgeTooHigh(){
+    assertTrue(Utils.isValidAge(150));
+  }
+  
+}
