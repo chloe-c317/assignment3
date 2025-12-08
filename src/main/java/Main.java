004f24b9372f7acfdc9b413a454a49b40206cf98
chloe-c317 <chloe.chinault@gmail.com>
2025-12-08 11:07:52 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,7 +20,7 @@ public class Main {
         System.out.println("Average GPA: " + service.calculateAverageGpa());
 
         // Dead code / unused
-        int x = 0;
+        int x = 42;
         if (x > 100) {
             System.out.println("Impossible");  // Never executes
         }
